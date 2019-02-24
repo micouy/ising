13bd0ba022ef120fdc21fc83d8ee55f46f98ca31
micouy <szpontaniczny@gmail.com>
2019-02-24 22:41:58 +0100

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+//! Everything you need to run a simplified
+//! [Ising model](https://en.wikipedia.org/wiki/Ising_model) simulation.
+//! In this model every spin's value must be either `-1` or `1`.
+//! [`ndarray`][ndarray]'s arrays are used to store spin lattice
+//! and perform computations. The lattice can either be generated
+//! by [`rand`][rand]s RNG or provided by the user.
+
 pub mod lattice;
