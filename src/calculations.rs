d145f30395ad72b47dcccbf98cf19afd99cae682
micouy <szpontaniczny@gmail.com>
2019-02-26 21:38:55 +0100

    Add and test calculating magnetic susceptibility

--- src/calculations.rs
+++ src/calculations.rs
@@ -8,10 +8,22 @@ fn calc_fluctuations(Es: Vec<u64>) -> f64 {
     avg_E_sq - avg_E.powi(2)
 }
 
+fn calc_mag_susceptibility(Is: Vec<f32>) -> f32 {
+    let n = Is.len() as f32;
+    let avg_I_sq = (Is.iter().fold(0.0, |sum, I| sum + I.powi(2)) as f32) / n;
+    let avg_I = (Is.iter().fold(0.0, |sum, I| sum + I) as f32) / n;
+
+    avg_I_sq - avg_I.powi(2)
+}
+
 #[cfg(test)]
 mod test {
     use ::pretty_assertions::assert_eq;
     use super::*;
+
+    fn float_error(x: f64, t: f64) -> f64 {
+        (x - t).abs() / t
+    }
 
     #[test]
     fn test_calculate_fluctuations() {
@@ -20,5 +32,14 @@ mod test {
         let fluctuations = calc_fluctuations(Es);
 
         assert_eq!(fluctuations, 9.5);
+    }
+
+    #[test]
+    fn test_caluculate_magnetic_susceptibility() {
+        let Is = vec![0.2, 0.4, 0.6, 0.8];
+
+        let mag_susceptibility = calc_mag_susceptibility(Is);
+
+        assert!(float_error(mag_susceptibility as f64, 0.05) < 0.01);
     }
 }
